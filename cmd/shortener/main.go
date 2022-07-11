d4d7cf5cbd7193fc55678c011fe32ff0caee3177
usa4ev <usa4ev@gmail.com>
2022-07-11 18:18:48 +0300

    minor fix

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,55 @@
 package main
 
-func main() {}
+import (
+	shortner "github.com/usa4ev/urlshortner/internal/app"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
+)
+
+func main() {
+
+	listen()
+
+}
+
+func listen() {
+	http.HandleFunc("/", rootHandler)
+	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func rootHandler(w http.ResponseWriter, r *http.Request) {
+
+	switch r.Method {
+	case http.MethodPost:
+
+		URL, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		id := shortner.ShortURL(string(URL))
+
+		_, err = io.WriteString(w, strconv.Itoa(id))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+	case http.MethodGet:
+		id, err := strconv.Atoi(r.URL.Path[1:])
+		if err != nil {
+			http.Error(w, "id must be an integer", http.StatusBadRequest)
+			return
+		}
+
+		redirect := shortner.GetPath(id)
+		if redirect == "" {
+			http.NotFound(w, r)
+			return
+		}
+		http.Redirect(w, r, redirect, http.StatusMovedPermanently)
+	}
+}
--- internal/app/app.go
+++ internal/app/app.go
@@ -1,5 +1,25 @@
-package main
+package app
 
-func main() {
+var urlMap map[int]string
+var i int
 
+func ShortURL(URL string) int {
+
+	if urlMap == nil {
+		urlMap = make(map[int]string)
+	}
+
+	for i, v := range urlMap {
+		if v == URL {
+			return i
+		}
+	}
+
+	i++
+	urlMap[i] = URL
+	return i
+}
+
+func GetPath(id int) string {
+	return urlMap[id]
 }
