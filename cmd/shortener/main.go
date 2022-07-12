ce9d170a74f32b2c9e2a65f35586a0721c31ff0a
usa4ev <usa4ev@gmail.com>
2022-07-12 16:04:47 +0300

    inc 3

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"github.com/go-chi/chi"
 	shortner "github.com/usa4ev/urlshortner/internal/app"
 	"io"
 	"log"
@@ -10,46 +11,49 @@ import (
 
 func main() {
 
-	listen()
+	r := chi.NewRouter()
+	r.Route("/", chiRouter)
+	log.Fatal(http.ListenAndServe("localhost:8080", r))
 
 }
 
-func listen() {
-	http.HandleFunc("/", rootHandler)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+func chiRouter(r chi.Router) {
+	r.Post("/", makeShort)
+	r.Get("/{id}", makeLong)
 }
 
-func rootHandler(w http.ResponseWriter, r *http.Request) {
-
-	switch r.Method {
-	case http.MethodPost:
-
-		URL, err := io.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		id := shortner.ShortURL(string(URL))
-
-		_, err = io.WriteString(w, strconv.Itoa(id))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-	case http.MethodGet:
-		id, err := strconv.Atoi(r.URL.Path[1:])
-		if err != nil {
-			http.Error(w, "id must be an integer", http.StatusBadRequest)
-			return
-		}
-
-		redirect := shortner.GetPath(id)
-		if redirect == "" {
-			http.NotFound(w, r)
-			return
-		}
-		http.Redirect(w, r, redirect, http.StatusMovedPermanently)
+func makeShort(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	URL, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
 	}
+
+	id := shortner.ShortURL(string(URL))
+
+	_, err = io.WriteString(w, strconv.Itoa(id))
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+func makeLong(w http.ResponseWriter, r *http.Request) {
+	strid := r.URL.Path[1:]
+	if strid == "" {
+		http.Error(w, "id is required", http.StatusBadRequest)
+		return
+	}
+	id, err := strconv.Atoi(strid)
+	if err != nil {
+		http.Error(w, "id must be an integer", http.StatusBadRequest)
+		return
+	}
+
+	redirect := shortner.GetPath(id)
+	if redirect == "" {
+		http.NotFound(w, r)
+		return
+	}
+	http.Redirect(w, r, redirect, http.StatusMovedPermanently)
 }
---
+++ cmd/shortener/main_test.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"bytes"
+	"github.com/go-chi/chi"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var host string
+
+func Test_rootHandler(t *testing.T) {
+	type args struct {
+		id string
+	}
+	type want struct {
+		response string
+		code     int
+	}
+	type data []struct {
+		id  string
+		url string
+	}
+	tests := []struct {
+		name string
+		args args
+		want want
+	}{
+		{"wrong id",
+			args{id: "5"},
+			want{"",
+				404},
+		},
+		{"ok",
+			args{id: "1"},
+			want{"",
+				301},
+		},
+		{"id is not int",
+			args{id: "b"},
+			want{"id must be an integer\n",
+				400},
+		},
+	}
+	d := data{{"1", host + "/test"},
+		{"2", host + "/test2"}}
+
+	//check if it works fine with empty db
+
+	r := chi.NewRouter()
+	r.Route("/", chiRouter)
+	ts := httptest.NewServer(r)
+	cl := ts.Client()
+	cl.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+	host = ts.URL
+	defer ts.Close()
+	res, err := cl.Get(host + "/1")
+	require.NoError(t, err)
+
+	if res.StatusCode != http.StatusNotFound {
+		t.Errorf("failed before data added, returned %v", res.StatusCode)
+	}
+
+	//filling test data
+	for _, s := range d {
+		res, err := cl.Post(host, "text/plain", bytes.NewBuffer([]byte(s.url)))
+		require.NoError(t, err)
+		if res.StatusCode != http.StatusOK {
+			t.Errorf("failed when filling test data, returned %v", res.StatusCode)
+			continue
+		}
+		body, err := io.ReadAll(res.Body)
+		if err != nil {
+			t.Errorf("failed reading response when filling data: %v", err.Error())
+		}
+		if string(body) != s.id {
+			t.Errorf("failed when filling test data. Expecded id %v, got %v", s.id, string(body))
+		}
+		err = res.Body.Close()
+		require.NoError(t, err)
+	}
+
+	for _, tt := range tests {
+
+		t.Run(tt.name, func(t *testing.T) {
+			res, err := cl.Get(host + "/" + tt.args.id)
+			require.NoError(t, err)
+
+			assert.Equal(t, tt.want.code, res.StatusCode)
+			if tt.want.response != "" {
+
+				body, err := io.ReadAll(res.Body)
+				require.NoError(t, err)
+				assert.Equal(t, tt.want.response, string(body))
+				err = res.Body.Close()
+				require.NoError(t, err)
+			}
+		})
+	}
+}
