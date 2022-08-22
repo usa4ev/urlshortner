b366acac3ae1c15013fd6406463ac90bb2befc9c
usa4ev <usa4ev@gmail.com>
2022-08-22 15:29:48 +0300

    inc13 1:1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,27 +1,24 @@
 package main
 
 import (
-	"context"
 	"errors"
 	"fmt"
 	"github.com/go-chi/chi"
+	"github.com/usa4ev/urlshortner/internal/router"
 	"github.com/usa4ev/urlshortner/internal/shortener"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
 )
 
 func main() {
 	// The HTTP Server
 	myShortener := shortener.NewShortener()
 	//r := router.NewRouter(myShortener)
-	r := NewRouter(myShortener)
-	server := &http.Server{Addr: "localhost:8080", Handler: r}
+	r := router.NewRouter(myShortener)
+	server := &http.Server{Addr: ":8080", Handler: r}
 
-	// Server run context
-	serverCtx, serverStopCtx := context.WithCancel(context.Background())
 	fmt.Println("addr: " + myShortener.Config.SrvAddr())
 	// Listen for syscall signals for process to interrupt/quit
 	sig := make(chan os.Signal, 1)
@@ -30,34 +27,19 @@ func main() {
 	go func() {
 		<-sig
 
-		// Shutdown signal with grace period of 30 seconds
-		shutdownCtx, cancelCtx := context.WithTimeout(serverCtx, 30*time.Second)
-
-		go func() {
-			<-shutdownCtx.Done()
-			if shutdownCtx.Err() == context.DeadlineExceeded {
-				defer cancelCtx()
-				fmt.Println("graceful shutdown timed out.. forcing exit.")
-			}
-		}()
-
 		// Trigger graceful shutdown
 		myShortener.FlushStorage()
-		err := server.Shutdown(shutdownCtx)
-		if err != nil {
-			fmt.Println(err.Error())
-		}
-		serverStopCtx()
+		server.Close()
 	}()
 
 	// Run the server
 	err := server.ListenAndServe()
+
+	fmt.Printf("listen and serve err: %v\n", err)
+
 	if err != nil && !errors.Is(err, http.ErrServerClosed) {
 		panic(err.Error())
 	}
-
-	// Wait for server context to be stopped
-	<-serverCtx.Done()
 }
 
 func NewRouter(myShortener *shortener.MyShortener) http.Handler {
